66059b0e9bb29c3e512466fd9f24994b639b8229
Bruce Hill <bhill@slac.stanford.edu>
2019-04-10 20:33:07 -0700

    Cleanup setup script replacing hard paths w/ macros

--- pcds_setup_py36env.sh
+++ pcds_setup_py36env.sh
@@ -2,11 +2,13 @@
 
 source /reg/g/pcds/package/conda/py36env.sh
 conda activate pva-gw
-source /reg/g/pcds/setup/epicsenv-bleeding_edge-7.0.sh 
+source $SETUP_SITE_TOP/epicsenv-bleeding_edge-7.0.sh 
+source $PSPKG_ROOT/etc/add_env_pkg.sh procServ/2.7.0-1.3.0
 
-PY_LD_VER=`python get_PY_LD_VER.py`
+P4P_TOP=/reg/neh/home/bhill/git-wa-neh/extensions/p4p-git 
+PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
 
 #echo pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
 #source /reg/g/pcds/setup/pathmunge.sh
-pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
 
