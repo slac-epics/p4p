d2d2ead44ffff6b57d1b1e37dedc3051a59b02b6
Bruce Hill <bhill@slac.stanford.edu>
2019-04-10 18:22:50 -0700

    Adding some pcds test support scripts

---
+++ pcds_setup_py36env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source /reg/g/pcds/package/conda/env-miniconda3.sh
+conda activate pva-gw
+source $SETUP_SITE_TOP/epicsenv-bleeding_edge-7.0.sh 
+source $PSPKG_ROOT/etc/add_env_pkg.sh procServ/2.7.0-1.3.0
+
+P4P_TOP=/reg/neh/home/bhill/git-wa-neh/extensions/p4p-git 
+PY_LD_VER=`python $P4P_TOP/get_PY_LD_VER.py`
+
+#echo pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+#source /reg/g/pcds/setup/pathmunge.sh
+pythonpathmunge $P4P_TOP/python${PY_LD_VER}/${EPICS_HOST_ARCH}
+
