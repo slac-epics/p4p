423a359afa2286dd1d7f532d3240f313fab6b98d
Hugo Slepicka <slepicka@slac.stanford.edu>
2020-04-14 14:26:24 -0700

    Updating to latest p4p and adding script for Python 2.7.13 environment setup.

--- lcls_setup_py27.sh
+++ lcls_setup_py27.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source $EPICS_SETUP/epicsenv-7.0.3.1-1.0.bash
-source $PACKAGE_TOP/python/3.6.1/rhel6-x86_64/use.bash
+source $TOOLS/python/go_python2.7.13.bash
 
-PY_LD_VER=3.6m
+PY_LD_VER=2.7
 
 #echo pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
 #source /reg/g/pcds/setup/pathmunge.sh
