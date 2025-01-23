a1c32e10b8b399b6709e932b30c7ef5851edd6e0
Jeremy Lorelli <lorelli@slac.stanford.edu>
2025-01-22 16:37:20 -0800

    Add lcls_setup script for python 3.10, remove old ones

--- lcls_setup_py36.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source $EPICS_SETUP/epicsenv-7.0.3.1-1.0.bash
-source $PACKAGE_TOP/python/3.6.1/rhel6-x86_64/use.bash
-
-PY_LD_VER=3.6m
-
-pythonpathmunge $PWD/python${PY_LD_VER}/${EPICS_HOST_ARCH}
