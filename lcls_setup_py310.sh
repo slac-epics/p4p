25c6687d4d216e7046579ecfd1d965a0dac02423
Jeremy Lorelli <lorelli@slac.stanford.edu>
2025-09-16 07:54:42 -0700

    Switch to Python 3.12 conda env

--- lcls_setup_py310.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Setup using the new Python 3.10 conda env
-source $PACKAGE_SITE_TOP/anaconda/envs/python3.10envs/v1.0/bin/activate
-
---
+++ lcls_setup_py312.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Setup using the new Python 3.12 conda env
+source $PACKAGE_SITE_TOP/anaconda/envs/python3.12envs/rhel7/rhel7_devel_rl/bin/activate
